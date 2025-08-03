ea8c71bdbace206e64a34c3090c7520d4b8f0d06
hyeon <hyeon.marina@gmail.com>
2025-08-03 10:35:52 +0900

    [Bronze I] Title: 평균, Time: 292 ms, Memory: 26196 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        double[] scores = new double[N];
+        double max = 0, sum = 0;
+        for (int i = 0; i < N; i++) {
+            scores[i] = sc.nextDouble();
+            if (scores[i] > max) max = scores[i];
+        }
+        for (int i = 0; i < N; i++) {
+            sum += scores[i] / max * 100;
+        }
+        System.out.println(sum / N);
+    }
+}

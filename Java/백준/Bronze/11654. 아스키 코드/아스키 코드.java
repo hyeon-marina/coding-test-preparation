1c589ca697968f71c990b707c243242f236d318c
hyeon <hyeon.marina@gmail.com>
2025-08-05 23:38:42 +0900

    [Bronze V] Title: 아스키 코드, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

--- Java/백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ Java/백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int code = sc.next().charAt(0);
+    public static void main(String[] args) throws IOException {
+        int code = System.in.read();
         System.out.println(code);
     }
 }

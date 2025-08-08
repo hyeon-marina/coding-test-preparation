d80cc36adc445b94bc93371951873a907b4ea7da
hyeon <hyeon.marina@gmail.com>
2025-08-08 20:26:59 +0900

    [Bronze II] Title: 단어의 개수, Time: 460 ms, Memory: 31536 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,13 @@
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+
+        StringTokenizer st = new StringTokenizer(s, " ");
+        System.out.println(st.countTokens());
+    }
+}

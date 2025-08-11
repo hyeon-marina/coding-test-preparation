6a6229d8d48f6cc7276a7ea78a98e8fe5189c31a
hyeon <hyeon.marina@gmail.com>
2025-08-11 20:34:40 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 112 ms, Memory: 14332 KB -BaekjoonHub

---
+++ Java/백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine();
+            if (isGroupWord(word)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static boolean isGroupWord(String word) {
+        boolean[] visited = new boolean[26];
+        char prev = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            char curr = word.charAt(i);
+            if (curr != prev) {
+                if (visited[curr - 'a']) {
+                    return false;
+                }
+                visited[curr - 'a'] = true;
+                prev = curr;
+            }
+        }
+        return true;
+    }
+}

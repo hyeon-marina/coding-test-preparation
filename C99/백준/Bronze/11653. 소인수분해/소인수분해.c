1fde0b311b33b70c5328f359ec3b976d204c199b
Hayeon Kwon <hyeon.marina@gmail.com>
2025-08-18 19:32:50 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/11653. 소인수분해/소인수분해.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    for (int i = 2; i * i <= N; i++) {
+        while (N % i == 0) {
+            printf("%d\n", i);
+            N /= i;
+        }
+    }
+    if (N > 1) {
+        printf("%d\n", N);
+    }
+    return 0;
+}

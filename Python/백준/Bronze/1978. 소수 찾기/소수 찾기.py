366f8d9e5aad4219700baaa248562f98e63a068f
Hayeon Kwon <hyeon.marina@gmail.com>
2025-08-18 19:24:30 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+N = int(input())
+nums = list(map(int, input().split()))
+count = 0
+for num in nums:
+    if is_prime(num):
+        count += 1
+print(count)

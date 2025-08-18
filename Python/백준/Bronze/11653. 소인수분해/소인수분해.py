cccb8f0abfbbf0e030d6e65c90a93828e77e6c1d
Hayeon Kwon <hyeon.marina@gmail.com>
2025-08-18 19:30:06 +0900

    [Bronze I] Title: 소인수분해, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+if n != 1:
+    i = 2
+    while i*i <= n:
+        while n % i == 0:
+            print(i)
+            n //= i
+        i += 1
+    if n > 1:
+        print(n)

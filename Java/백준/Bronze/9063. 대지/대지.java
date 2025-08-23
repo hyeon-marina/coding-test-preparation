7310e2ea87f6ae8c7a8e33bb14382d92a31380ca
Hayeon Kwon <hyeon.marina@gmail.com>
2025-08-24 08:37:13 +0900

    [Bronze III] Title: 대지, Time: 248 ms, Memory: 26904 KB -BaekjoonHub

---
+++ Java/백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int minX = 10001, maxX = -10001;
+        int minY = 10001, maxY = -10001;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if (x < minX) minX = x;
+            if (x > maxX) maxX = x;
+            if (y < minY) minY = y;
+            if (y > maxY) maxY = y;
+        }
+
+        if (n == 1) System.out.print(0);
+        else System.out.print((maxX - minX) * (maxY - minY));
+    }
+}

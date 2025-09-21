ad407f3d80e73edd8e29f89847b185c9dc55a4db
Hayeon Kwon <hyeon.marina@gmail.com>
2025-09-21 10:50:00 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    long long result = n * n * n;
+    cout << result << "\n";
+    cout << 3 << "\n";
+    return 0;
+}

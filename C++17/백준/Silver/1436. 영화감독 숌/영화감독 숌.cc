1f27f1946d2f92120581dbb8e917e2cc5c7badc7
Hayeon Kwon <hyeon.marina@gmail.com>
2025-10-04 20:07:55 +0900

    [Silver V] Title: 영화감독 숌, Time: 60 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+    int count = 0;
+    int num = 666;
+    while (true) {
+        string s = to_string(num);
+        if (s.find("666") != string::npos) {
+            count++;
+            if (count == N) {
+                cout << num << "\n";
+                break;
+            }
+        }
+        num++;
+    }
+    return 0;
+}

d6b87452fc0e36bd9c403cd620545cbc7ce8aa86
Hayeon Kwon <hyeon.marina@gmail.com>
2025-10-18 20:05:14 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, k;
+    cin >> N >> k;
+    vector<int> scores(N);
+    for (int i = 0; i < N; i++) {
+        cin >> scores[i];
+    }
+    sort(scores.begin(), scores.end(), greater<int>());
+    cout << scores[k-1] << "\n";
+    return 0;
+}

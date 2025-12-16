41ce17665fe01ae89727c0fbe7e616377f830a4d
Hayeon Kwon <hyeon.marina@gmail.com>
2025-12-16 19:57:52 +0900

    [level 3] Title: 산 모양 타일링, Time: 6.16 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n, int[] tops) {
+        final int MOD = 10007;
+
+        if (n == 1) return (tops[0] == 1) ? 3 : 2;
+
+        int dp0 = 1;
+        int dp1 = (tops[0] == 1) ? 3 : 2; // dp[1]
+
+        for (int i = 2; i <= 2 * n; i++) {
+            int cur;
+            if ((i & 1) == 1 && tops[i / 2] == 1) {
+                cur = (dp1 * 2 + dp0) % MOD;
+            } else {
+                cur = (dp1 + dp0) % MOD;
+            }
+            dp0 = dp1;
+            dp1 = cur;
+        }
+        return dp1;
+    }
+}

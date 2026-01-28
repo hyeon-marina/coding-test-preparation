9bef7fb18a191cd07e9447b5bf17de707b89257c
Hayeon Kwon <hyeon.marina@gmail.com>
2026-01-28 19:54:44 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Silver/13241. 최소공배수/최소공배수.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+long long gcd_ll(long long a, long long b) {
+    while (b != 0) {
+        long long t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
+
+int main(void) {
+    long long a, b;
+    if (scanf("%lld %lld", &a, &b) != 2) return 0;
+
+    long long g = gcd_ll(a, b);
+    long long lcm = (a / g) * b;
+    printf("%lld\n", lcm);
+    return 0;
+}

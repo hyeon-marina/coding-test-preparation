d1d58dee46a842bc7bcfab94cfb8710c379c458f
Hayeon Kwon <hyeon.marina@gmail.com>
2026-01-28 19:53:10 +0900

    [Silver V] Title: 최소공배수, Time: 100 ms, Memory: 14288 KB -BaekjoonHub

---
+++ Java/백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        long g = gcd(a, b);
+        long lcm = (a / g) * b;
+        System.out.println(lcm);
+    }
+}

96784a4df3fcb5717119dbd2ff25456228b99003
Hayeon Kwon <hyeon.marina@gmail.com>
2026-02-08 23:14:52 +0900

    [Silver III] Title: 소수 구하기, Time: 8 ms, Memory: 2272 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m, n;
+    cin >> m >> n;
+
+    vector<bool> isPrime(n + 1, true);
+    if (n >= 0) isPrime[0] = false;
+    if (n >= 1) isPrime[1] = false;
+
+    int limit = (int)sqrt((double)n);
+    for (int i = 2; i <= limit; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    for (int x = max(2, m); x <= n; x++) {
+        if (isPrime[x]) cout << x << '\n';
+    }
+    return 0;
+}
